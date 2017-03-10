67b9a453ffd82d0a925c7e5f74824551ba7e1e80
oguzhanarslan <oguzhan28@hotmail.nl>
2017-03-10 14:25:48 +0100

    no message

--- index.php
+++ index.php
@@ -4,34 +4,46 @@ include_once( "./class.TemplatePower.inc.php" );
 $tpl = new TemplatePower( "./tpl/index.tpl.html" );
 $tpl->prepare();
 
-  // maak een verbinding met mysql
+$months =['onbekend','januari','februari','maart','april','mei','juni','juli','augustus',
+                   'september','oktober','november','december'];
+
+
+  // maak een verbinding met mysqli
   $connection = mysqli_connect('localhost', 'root','');
 	
   // geef aan welke database we willen gebruiken
-  mysqli_select_db($connection, "year2");
-  
+  mysqli_select_db($connection, 'calendar');
+
   // maak een query om alle spellen op te vragen
-  $query = "select * from Calender order by Birthday_ID";
-  	
+  $query = "select * from birthdays order by month, day";
+  
   // voer de query uit
   $result = mysqli_query($connection, $query);
   // loop door alle rijen heen
+$lastmonth = -1;
+
 while ($row = mysqli_fetch_assoc ($result))
 	
 {
-    $Birthday_ID = $row["Birthday_ID"];
-    $Firstname = $row["Firstname"];
-    $Lastname = $row["Lastname"];
-    $Age = $row["Age"];
-    
-    $tpl->newBlock( "calender");
-    $tpl->assign( "Birthday_ID", $Birthday_ID );
-    $tpl->newBlock( "calender_birthdays");
-    $tpl->assign( "Firstname", $Firstname);
-    $tpl->assign( "Lastname", $Lastname);
-    $tpl->assign( "Age", $Age);
-    }
-
+	// haal gegevens die we nodig hebben uit de rij
+	$id = $row["id"];
+	$person = $row["person"];
+	$day = $row["day"];
+	$month = $row["month"];
+	$year = $row["year"];
+	
+	$tpl->newBlock( "birthdays");
+	$tpl->assign( "id", $id );
+	$tpl->assign( "person", $person );
+    $tpl->assign( "day", $day );
+    if($lastmonth == $month){
+        $tpl->assign( "month","");
+    }else {
+        $tpl->assign( "month", $months[$month]);
+    };
+     $tpl->assign( "year", $year );
+    $lastmonth = $month;
+}
 
 $tpl->gotoBlock( "_ROOT" );
 $tpl->printToScreen();
